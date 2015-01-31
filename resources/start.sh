122c5beceb9b896b8b62c519a978c6b5b4467a01
Shaun M <shaun@samsite.ca>
2015-01-31 01:01:14 -0800

    Update start.sh

--- resources/start.sh
+++ resources/start.sh
@@ -9,7 +9,20 @@ if [ ! -f /var/log/foreman-install.log ]; then
   else
     echo 'Starting interactive installer'
     foreman-installer -iv
+    if [ $? -ne 0 ]; then exit $?; fi
   fi
+fi
+
+# install docker plugin for foreman
+# relax, you don't have to use it if you don't want it.
+if [ ! `dpkg -l | grep -E '^ii' | grep ruby-foreman-docker` ]; then
+  apt-get install -y ruby-foreman-docker
+fi
+
+# correct for foreman-installer bug
+# see: http://projects.theforeman.org/issues/8915
+if [ -f /etc/puppet/puppet.conf ]; then
+  sed -i '/trusted_node_data/d' /etc/puppet/puppet.conf
 fi
 
 # normal startup
